be311569bf8286c9df463c7a61739316b1c82e2a
nghiatd987 <nghiatd987@gmail.com>
2025-03-22 16:30:33 +0700

    [BE] add linkedin:crawl command

---
+++ app/Console/Commands/Kernel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+use App\Console\Commands\CrawlLinkedinData;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // Example: Run a command daily
+        // $schedule->command('inspire')->daily();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+
+        // Register custom commands
+        $this->commands([
+            CrawlLinkedinData::class,
+        ]);
+
+    }
+}
